718693b44638de616744467c7707df3fa32c4ccf
parasjogani <paras.jogani@gmail.com>
2023-05-31 16:45:55 +0530

    answer1 added

---
+++ Assignment 1/answer1.js
@@ -0,0 +1,24 @@
+
+// Answer 1
+function sum(nums, target) {
+    const newnums = {};
+
+    for(let i=0;i<nums.length;i++){
+        const subNum = target - nums[i];
+
+        if(subNum in newnums){
+            return [newnums[subNum], i];
+        }
+        newnums[nums[i]] = i;
+    }
+
+    return []; //if no solution
+}
+
+
+//Example:
+
+const nums = [1, 5, 9, 10, 13, 16];
+const target = 26;
+const result = sum(nums, target);
+console.log(result);
