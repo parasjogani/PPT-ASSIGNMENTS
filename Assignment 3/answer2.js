3b52b7bc4ec6ec788f5e506d86f0e34a05ca1073
parasjogani <paras.jogani@gmail.com>
2023-06-02 22:20:03 +0530

    answer2 added

---
+++ Assignment 3/answer2.js
@@ -0,0 +1,57 @@
+
+// Answer 2
+function fourSum(nums, target) {
+    nums.sort((a, b) => a - b); // Sorting array
+
+    const quadru = [];
+    const n = nums.length;
+
+    for (let i = 0; i < n - 3; i++) {
+        // Skip duplicates
+        if (i > 0 && nums[i] === nums[i - 1]) {
+            continue;
+        }
+
+        for (let j = i + 1; j < n - 2; j++) {
+            // Skip duplicates
+            if (j > i + 1 && nums[j] === nums[j - 1]) {
+                continue;
+            }
+
+            let left = j + 1;
+            let right = n - 1;
+
+            while (left < right) {
+                const sum = nums[i] + nums[j] + nums[left] + nums[right];
+
+                if (sum === target) {
+                    quadru.push([nums[i], nums[j], nums[left], nums[right]]);
+
+                    // Skip duplicates
+                    while (left < right && nums[left] === nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] === nums[right - 1]) {
+                        right--;
+                    }
+
+                    left++;
+                    right--;
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+    }
+
+    return quadru;
+}
+
+// Example
+const nums = [1, 0, -1, 0, -2, 2];
+const target = 0;
+
+const result = fourSum(nums, target);
+console.log(result); // [ [ -2, -1, 1, 2 ], [ -2, 0, 0, 2 ], [ -1, 0, 0, 1 ] ]
