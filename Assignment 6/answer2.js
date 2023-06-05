59cd004dd360c63680af6c5b685bc8b593b0b5f4
parasjogani <paras.jogani@gmail.com>
2023-06-05 18:15:39 +0530

    answer2 added

--- Assignment 6/answer2.js
+++ Assignment 6/answer2.js
@@ -32,6 +32,6 @@ function searchMatrix(matrix, target) {
 
 let matrix = [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]];
 let target = 3;
-console.log(searchMatrix(matrix, target))
+console.log(searchMatrix(matrix, target)) // True
 
 
