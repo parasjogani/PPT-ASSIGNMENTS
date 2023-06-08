06f80f8ea7357157d915ad0e23938a9b855dcaa5
parasjogani <paras.jogani@gmail.com>
2023-06-08 17:02:39 +0530

    answer3 added

---
+++ Assignment 8/answer3.js
@@ -0,0 +1,37 @@
+
+// Answer 3
+function minStepsToDelete(word1, word2) {
+    const m = word1.length;
+    const n = word2.length;
+
+    const dp = new Array(m + 1);
+    for (let i = 0; i <= m; i++) {
+        dp[i] = new Array(n + 1).fill(0);
+    }
+
+    for (let i = 1; i <= m; i++) {
+        dp[i][0] = i;
+    }
+    for (let j = 1; j <= n; j++) {
+        dp[0][j] = j;
+    }
+
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (word1.charAt(i - 1) === word2.charAt(j - 1)) {
+                dp[i][j] = dp[i - 1][j - 1];
+            } else {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + 1;
+            }
+        }
+    }
+
+    return dp[m][n];
+}
+
+
+// Example
+const word1 = "sea";
+const word2 = "eat";
+const steps = minStepsToDelete(word1, word2);
+console.log(steps); // 2
