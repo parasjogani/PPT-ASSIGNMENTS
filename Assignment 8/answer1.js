53773601dbbf069e052923882b9a19f93b3386c1
parasjogani <paras.jogani@gmail.com>
2023-06-08 16:50:25 +0530

    answer1 added

---
+++ Assignment 8/answer1.js
@@ -0,0 +1,43 @@
+
+// Answer 1
+function minimumDeleteSum(s1, s2) {
+    const m = s1.length;
+    const n = s2.length;
+
+    // Create a 2D array dp
+    const dp = new Array(m + 1);
+    for (let i = 0; i <= m; i++) {
+        dp[i] = new Array(n + 1).fill(0);
+    }
+
+    // Initialize the first row and the first column
+    for (let i = 1; i <= m; i++) {
+        dp[i][0] = dp[i - 1][0] + s1.charCodeAt(i - 1);
+    }
+    for (let j = 1; j <= n; j++) {
+        dp[0][j] = dp[0][j - 1] + s2.charCodeAt(j - 1);
+    }
+
+    // Compute the remaining cells of dp
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (s1.charAt(i - 1) === s2.charAt(j - 1)) {
+                dp[i][j] = dp[i - 1][j - 1];
+            } else {
+                dp[i][j] = Math.min(
+                    dp[i - 1][j] + s1.charCodeAt(i - 1),
+                    dp[i][j - 1] + s2.charCodeAt(j - 1)
+                );
+            }
+        }
+    }
+
+    return dp[m][n];
+}
+
+// Example
+const s1 = "sea";
+const s2 = "eat";
+const result = minimumDeleteSum(s1, s2);
+console.log(result); // 231
+
