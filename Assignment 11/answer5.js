6e09bfe1bbe1177c515089df9d402b9dca7adccf
parasjogani <paras.jogani@gmail.com>
2023-06-13 15:44:46 +0530

    answer5 added

--- Assignment 11/answer5.js
+++ Assignment 11/answer5.js
@@ -1,24 +1,45 @@
 
 // Answer 5
+function binarySearch(nums, target) {
+    let low = 0;
+    let high = nums.length - 1;
+
+    while (low <= high) {
+        const mid = Math.floor((low + high) / 2);
+
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
 function intersection(nums1, nums2) {
-    const set = new Set();
+    nums1.sort((a, b) => a - b);
+    nums2.sort((a, b) => a - b);
+
     const intersection = [];
 
     for (const num of nums1) {
-        set.add(num);
-    }
-
-    for (const num of nums2) {
-        if (set.has(num)) {
+        if (
+            binarySearch(nums2, num) !== -1 &&
+            intersection.indexOf(num) === -1
+        ) {
             intersection.push(num);
-            set.delete(num);
         }
     }
 
     return intersection;
 }
 
+
 // Example
 const nums1 = [1, 2, 2, 1];
 const nums2 = [2, 2];
 console.log(intersection(nums1, nums2)); // [2]
+
