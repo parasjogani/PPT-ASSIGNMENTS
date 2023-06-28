e235a9492aa1b7b9d99275a994791eeb7972ae31
parasjogani <paras.jogani@gmail.com>
2023-06-28 20:35:33 +0530

    answer2 added

---
+++ Assignment 18/answer2.js
@@ -0,0 +1,31 @@
+
+// Answer 2
+function sortColors(nums) {
+    let low = 0; 
+    let mid = 0;
+    let high = nums.length - 1;
+
+    while (mid <= high) {
+        if (nums[mid] === 0) {
+            swap(nums, low, mid);
+            low++;
+            mid++;
+        } else if (nums[mid] === 1) {
+            mid++;
+        } else if (nums[mid] === 2) {
+            swap(nums, mid, high);
+            high--;
+        }
+    }
+}
+
+function swap(nums, i, j) {
+    const temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+}
+
+// Example
+const nums = [2, 0, 2, 1, 1, 0];
+sortColors(nums);
+console.log(nums); // [ 0, 0, 1, 1, 2, 2 ]
